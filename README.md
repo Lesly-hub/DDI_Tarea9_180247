103a07d0c97e02869fd0ed25efa9039a6f6e489e
Lesly_hub <lesly.yareth150201@gmail.com>
2023-07-18 23:59:37 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# DDI_TAREA9_180247
+
+**Implementacion del geopocisionamiento en dispositivos Wereables**
+
+**Nombre:Lesly Yareth Hernandez Bonilla**
+ **Matricula:180247**
+ **Materia: Desarrollo para Dispositivos Inteligentes**
+ **Docente: M.T.I Marco A. Ramirez Hernandez**
---
+++ app/src/main/java/mx/edu/utxj/ti/idgs/ddi/tarea9_180247/presentation/theme/Theme.kt
@@ -0,0 +1,17 @@
+package mx.edu.utxj.ti.idgs.ddi.tarea9_200687.presentation.theme
+
+import androidx.compose.runtime.Composable
+import androidx.wear.compose.material.MaterialTheme
+
+@Composable
+fun tarea9_200687Theme(
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        colors = wearColorPalette,
+        typography = Typography,
+        // For shapes, we generally recommend using the default Material Wear shapes which are
+        // optimized for round and non-round devices.
+        content = content
+    )
+}
